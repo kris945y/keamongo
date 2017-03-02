d7d1b2dfe12e2a471d63663aac61e3c266b3d6e8
Kristian Kromand <kristiankromand@Kristians-MacBook-Pro-2.local>
2017-03-02 11:14:05 +0100

    samme som før

--- googlemaps.js
+++ googlemaps.js
@@ -1,23 +1,20 @@
-window.addEventListener("load", initMap);
+   var map;
 
+   function initMap() {
+       map = new google.maps.Map(document.getElementById('map'), {
+           center: {
+               lat: 55.706415,
+               lng: 12.538865
+           },
+           zoom: 17
+       });
 
-var map;
-
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 55.706415,
-            lng: 12.538865
-        },
-        zoom: 17
-    });
-
-    var marker = new google.maps.Marker({
-        position: {
-            lat: 55.706415,
-            lng: 12.538865
-        },
-        map: map,
-        title: 'Hello World!'
-    });
-}
+       var marker = new google.maps.Marker({
+           position: {
+               lat: 55.706415,
+               lng: 12.538865
+           },
+           map: map,
+           title: 'Hello World!'
+       });
+   }
